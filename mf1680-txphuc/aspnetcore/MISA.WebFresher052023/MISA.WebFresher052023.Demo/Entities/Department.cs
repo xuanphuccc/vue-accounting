001a912a3bd1171593d02cb1c8ce1d99ba851ee6
xuanphuc <dev.xuanphuc@gmail.com>
2023-07-13 08:52:12 +0700

    fix: Thêm comments

--- mf1680-txphuc/aspnetcore/MISA.WebFresher052023/MISA.WebFresher052023.Demo/Entities/Department.cs
+++ mf1680-txphuc/aspnetcore/MISA.WebFresher052023/MISA.WebFresher052023.Demo/Entities/Department.cs
@@ -4,27 +4,35 @@ namespace MISA.WebFresher052023.Demo.Entities
 {
     public class Department
     {
+        // Khoá chính
         [Required]
         public Guid DepartmentId { get; set; }
 
+        // Mã đơn vị
         [Required]
         [StringLength(20)]
         public string DepartmentCode { get; set; } = string.Empty;
 
+        // Tên đơn vị
         [Required]
         [StringLength(255)]
         public string DepartmentName { get; set; } = string.Empty;
 
+        // Mô tả
         [StringLength(255)]
         public string? Description { get; set; }
 
+        // Ngày tạo
         public DateTime? CreatedDate { get; set; }
 
+        // Người tạo
         [StringLength(255)]
         public string? CreatedBy { get; set; }
 
+        // Ngày sửa
         public DateTime? ModifiedDate { get; set; }
 
+        // Người sửa
         [StringLength(255)]
         public string? ModifiedBy { get; set; }
     }
--- mf1680-txphuc/aspnetcore/MISA.WebFresher052023/MISA.WebFresher052023.Demo/Entities/Position.cs
+++ mf1680-txphuc/aspnetcore/MISA.WebFresher052023/MISA.WebFresher052023.Demo/Entities/Position.cs
@@ -4,27 +4,35 @@ namespace MISA.WebFresher052023.Demo.Entities
 {
     public class Position
     {
+        // Khoá chính
         [Required]
         public Guid PositionId { get; set; }
 
+        // Mã vị trí
         [Required]
         [StringLength(20)]
         public string PositionCode { get; set; } = string.Empty;
 
+        // Tên vị trí
         [Required]
         [StringLength(255)]
         public string PositionName { get; set; } = string.Empty;
 
+        // Mô tả
         [StringLength(255)]
         public string? Description { get; set; }
 
+        // Ngày tạo
         public DateTime? CreatedDate { get; set; }
 
+        // Người tạo
         [StringLength(255)]
         public string? CreatedBy { get; set; }
 
+        // Ngày sửa
         public DateTime? ModifiedDate { get; set; }
 
+        // Người sửa
         [StringLength(255)]
         public string? ModifiedBy { get; set; }
     }
