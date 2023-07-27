246b69741d505fbf405fe628acf6c269eb7a2927
xuanphuc <dev.xuanphuc@gmail.com>
2023-07-27 08:04:35 +0700

    docs: Cập nhật comments

--- mf1680-txphuc/amis-accounting/src/api/department-api.js
+++ mf1680-txphuc/amis-accounting/src/api/department-api.js
@@ -3,6 +3,10 @@ import axiosClient from "./axios-client";
 const rootUrl = "/Departments";
 
 const departmentApi = {
+  /**
+   * Description: Lấy danh sách toàn bộ đơn vị
+   * Author: txphuc (27/07/2023)
+   */
   getAll() {
     const url = rootUrl;
     return axiosClient.get(url);
--- mf1680-txphuc/amis-accounting/src/api/position-api.js
+++ mf1680-txphuc/amis-accounting/src/api/position-api.js
@@ -3,6 +3,10 @@ import axiosClient from "./axios-client";
 const rootUrl = "/Positions";
 
 const positionApi = {
+  /**
+   * Description: Lấy danh sách toàn bộ vị trí
+   * Author: txphuc (27/07/2023)
+   */
   getAll() {
     const url = rootUrl;
     return axiosClient.get(url);
